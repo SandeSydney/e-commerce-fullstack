62bc3a8a7fa50c56f65411557dc2aeb89b7e22e2
SandeSydney <sandesydney01@gmail.com>
2022-11-26 18:44:16 +0300

    Modified addOrUpdate routes

--- Backend/Routes/productRoutes.js
+++ Backend/Routes/productRoutes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
-const { getAllProducts, addOrUpdateProduct, getOneProduct, softDeleteProduct } = require('../Controller')
+const { getAllProducts, addOrUpdateProducts, getOneProduct, softDeleteProduct } = require('../Controller/productController')
 
 const router = Router()
 
 router.get('/', getAllProducts)
-router.post('/', addOrUpdateProducts)
+// router.post('/', addOrUpdateProducts)
 router.get('/:id', getOneProduct)
 router.put('/:id', addOrUpdateProducts)
 router.delete('/:id', softDeleteProduct)
