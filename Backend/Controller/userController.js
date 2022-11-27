44fcc7faf551642b99795c7948e8b54152ec5467
SandeSydney <sandesydney01@gmail.com>
2022-11-27 13:30:10 +0300

    Initial user controller

---
+++ Backend/Controller/userController.js
@@ -0,0 +1,20 @@
+const signUpUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = {
+    signUpUser,
+    loginUser
+}
