057b68273d490177feae98705cdd4041de824127
SandeSydney <sandesydney01@gmail.com>
2022-11-27 19:28:00 +0300

    Update product routes with middleware

--- Backend/Routes/productRoutes.js
+++ Backend/Routes/productRoutes.js
@@ -1,13 +1,22 @@
-const { Router } = require('express')
-const { getAllProducts, addOrUpdateProducts, getOneProduct, softDeleteProduct } = require('../Controller/productController')
+const {
+    Router
+} = require('express')
+const {
+    getAllProducts,
+    addOrUpdateProducts,
+    getOneProduct,
+    softDeleteProduct
+} = require('../Controller/productController')
+const {
+    tokenVerify
+} = require('../Middleware/tokenVerify')
 
 const router = Router()
 
-router.get('/', getAllProducts)
-router.post('/', addOrUpdateProducts)
-router.get('/:id', getOneProduct)
-// router.put('/:id', addOrUpdateProducts)
-router.delete('/:id', softDeleteProduct)
+router.get('/', tokenVerify, getAllProducts)
+router.post('/', tokenVerify, addOrUpdateProducts)
+router.get('/:id', tokenVerify, getOneProduct)
+router.delete('/:id', tokenVerify, softDeleteProduct)
 
 // Export the router. Has all methods
 module.exports = {
