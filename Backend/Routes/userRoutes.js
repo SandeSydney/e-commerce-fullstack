302a98e316870ad01c0e466453bc9ac3fd736761
SandeSydney <sandesydney01@gmail.com>
2022-11-27 18:53:49 +0300

    Added user routes

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,8 +4,8 @@ const { signUpUser, loginUser } = require('../Controller/userController')
 
 const userRoutes = Router()
 
-userRoutes.post("./signup", signUpUser)
-userRoutes.post("./login", loginUser)
+userRoutes.post("/signup", signUpUser)
+userRoutes.post("/login", loginUser)
 
 module.exports = {
     userRoutes
