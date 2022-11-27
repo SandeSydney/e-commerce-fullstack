66bd8e518c1f5f9b483cd2d93bca22b83e6909e0
SandeSydney <sandesydney01@gmail.com>
2022-11-27 13:28:26 +0300

    Procedure to add or update user

---
+++ Backend/Database/StoredProcedures/user/addOrUpdateUsers.sql
@@ -0,0 +1,27 @@
+CREATE OR ALTER PROCEDURE usp_InsertUpdateUser
+    (
+    @email VARCHAR(200),
+    @username VARCHAR(200),
+    @password VARCHAR(200),
+    @IsDeleted BIT = 0
+)
+AS
+BEGIN
+    IF EXISTS (SELECT *
+    FROM [dbo].[usersTable]
+    WHERE email = @email)
+        BEGIN
+        UPDATE dbo.usersTable SET 
+                username = @username,
+                password = @password
+                WHERE email = @email
+        PRINT 'User Updated Successfully!'
+    END
+    ELSE
+        BEGIN
+        INSERT INTO dbo.usersTable
+        VALUES
+            (@email, @username, @password, @IsDeleted)
+        PRINT 'User Added Successfully!'
+    END
+END
