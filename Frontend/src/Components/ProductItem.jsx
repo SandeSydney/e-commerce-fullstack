d3651a1aded175c66d677249161a9786ff345225
SandeSydney <sandesydney01@gmail.com>
2022-11-28 14:18:41 +0300

    Update of product item

--- Frontend/src/Components/ProductItem.jsx
+++ Frontend/src/Components/ProductItem.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addCartItem } from '../Features/cartSlice'
 
 function ProductItem({ product }) {
+  const dispatch = useDispatch()
+  const sendtocart = ()=>{
+    dispatch(addCartItem({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      image_url: product.image_url,
+      discount_rate: product.discount_rate,
+      quantity: 1
+    }))
+  }
+
   return (
     <div className='productItem'>
       <img src={product.image_url} alt="Product Item" />
@@ -8,7 +22,7 @@ function ProductItem({ product }) {
       <p>Ksh. {product.price}</p>
       <p>{product.description}</p>
       <p>{product.discount_rate}% Off!</p>
-      <button className='addToCartBtn'>Add to Cart</button>
+      <button className='addToCartBtn' onClick={sendtocart}>Add to Cart</button>
     </div>
   )
 }
