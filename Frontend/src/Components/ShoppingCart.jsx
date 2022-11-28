e62dd879f63709246123161bac17e972175aaf5a
SandeSydney <sandesydney01@gmail.com>
2022-11-28 04:08:17 +0300

    Updated the shopping cart structure

--- Frontend/src/Components/ShoppingCart.jsx
+++ Frontend/src/Components/ShoppingCart.jsx
@@ -47,13 +47,29 @@ function ShoppingCart() {
       <h2><u>Your Cart</u></h2>
       <div className="cartDisplay">
 
-        <div className="cartHead">Cart Head</div>
+        {/* <div className="cartHead">Cart Head</div> */}
 
-        {falseData.map((item, id) => {
-          return <CartItem key={item.id} item={item} />
-        })}
-
-        <div className="cartFooter">Cart Footer</div>
+        <div className="cartBody">
+          {(falseData.length) ?
+            falseData.map((item, id) => { return <CartItem key={item.id} item={item} /> }) :
+            <p>No items in cart. Click on a product to add one!</p>}
+        </div>
+        <div className="cartFooter">
+          <div className="emptyspace"></div>
+          <div className="totalsDiv">
+            <div className='totals'>
+              <p>Total Items: </p>
+              <p>Number</p>
+            </div>
+            <div className='totals'>
+              <p>Total Items: </p>
+              <p>Number</p>
+            </div>
+            <div className='pay'>
+              <button className='submitBtn'>Pay Now</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
