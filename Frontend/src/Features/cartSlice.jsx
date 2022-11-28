2aee27c994b56995bf57fb4830ec1395d7df23da
SandeSydney <sandesydney01@gmail.com>
2022-11-28 14:17:44 +0300

    Update on cart slice

--- Frontend/src/Features/cartSlice.jsx
+++ Frontend/src/Features/cartSlice.jsx
@@ -10,14 +10,33 @@ export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addCartItem: (state, action)=>{
-            state.content.push(action.payload)
-        }, 
-        removeCartItem: (state, action)=>{
-            return state.content.filter((item, idx)=>idx !== action.payload.index)
+        addCartItem: (state, action) => {
+            const itemInCart = state.content.find((item) => item.id === action.payload.id)
+            if (itemInCart) {
+                itemInCart.quantity++
+            } else {
+                state.content.push({ ...action.payload, quantity: 1 })
+            }
         },
-        increaseItemQuantity: (state, action)=>{
-            
+        removeCartItem: (state, action) => {
+            const newContent = state.content.filter((item) => item.id !== action.payload.id)
+            state.content = newContent
+        },
+        increaseItemQuantity: (state, action) => {
+            const item = state.content.find((item) => item.id === action.payload.id)
+            item.quantity++
+        },
+        decreaseItemQuantity: (state, action) => {
+            const item = state.content.find(item => item.id === action.payload.id)
+            if (item.quantity === 1) {
+                removeCartItem(item)
+            } else {
+                item.quantity--
+            }
         }
     }
 })
+
+export const selectAllCartItems = (state)=>state.cart.content
+export default cartSlice.reducer
+export const { addCartItem, increaseItemQuantity, decreaseItemQuantity, removeCartItem } = cartSlice.actions
