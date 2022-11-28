1d60d5b68de36d9e02e1362fcee63159021184c4
SandeSydney <sandesydney01@gmail.com>
2022-11-28 14:18:12 +0300

    Removed fake data and got real from state

--- Frontend/src/Components/ShoppingCart.jsx
+++ Frontend/src/Components/ShoppingCart.jsx
@@ -1,46 +1,34 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectAllCartItems } from '../Features/cartSlice'
 import CartItem from './CartItem'
 
 function ShoppingCart() {
 
-  const falseData = [
-    {
-      "id": 15,
-      "title": "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
-      "price": 56.99,
-      "description": "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-      "category": "women's clothing",
-      "image": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
-      "rating": {
-        "rate": 2.6,
-        "count": 235
+  const cartItems = useSelector(selectAllCartItems)
+
+  const gettotalitems = () => {
+    if (cartItems.length) {
+      let totalItems = 0
+      for (let key in cartItems) {
+        totalItems += cartItems[key].quantity
       }
-    },
-    {
-      "id": 16,
-      "title": "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
-      "price": 29.95,
-      "description": "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
-      "category": "women's clothing",
-      "image": "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
-      "rating": {
-        "rate": 2.9,
-        "count": 340
-      }
-    },
-    {
-      "id": 17,
-      "title": "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
-      "price": 39.99,
-      "description": "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
-      "category": "women's clothing",
-      "image": "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
-      "rating": {
-        "rate": 3.8,
-        "count": 679
-      }
+      return totalItems
+    } else {
+      return 0
     }
-  ]
+  }
+
+  const getTotalPrice = ()=>{
+    if(cartItems.length){
+      let totalPrice = 0
+      for(let key in cartItems){
+        let itemsPrice = (cartItems[key].price)*(cartItems[key].quantity)
+        totalPrice += itemsPrice
+      }
+      return totalPrice
+    }
+  }
 
   return (
     <div className='container'>
@@ -50,20 +38,20 @@ function ShoppingCart() {
         {/* <div className="cartHead">Cart Head</div> */}
 
         <div className="cartBody">
-          {(falseData.length) ?
-            falseData.map((item, id) => { return <CartItem key={item.id} item={item} /> }) :
-            <p>No items in cart. Click on a product to add one!</p>}
+          {(cartItems.length) ?
+            cartItems.map((item) => { return <CartItem key={item.id} item={item} /> }) :
+            <p className='noCartItems'>No items in cart. Click on a product to add one!</p>}
         </div>
         <div className="cartFooter">
           <div className="emptyspace"></div>
           <div className="totalsDiv">
             <div className='totals'>
-              <p>Total Items: </p>
-              <p>Number</p>
+              <p>Total Price: </p>
+              <p>{getTotalPrice()}</p>
             </div>
             <div className='totals'>
               <p>Total Items: </p>
-              <p>Number</p>
+              <p>{gettotalitems()}</p>
             </div>
             <div className='pay'>
               <button className='submitBtn'>Pay Now</button>
